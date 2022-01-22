91cfbf1860d220ef3b748e43a002068f26c3bfdb
nanase881 <s1290002@u-aizu.ac.jp>
2022-01-22 14:49:50 +0900

    Made title scene

---
+++ Assets/meiro/Scripts/sceneChange.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class sceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //For check button
+    //public void OnClick()
+    //{
+    //    Debug.Log("押された!");  // ログを出力
+    //}
+
+    //For changing scene
+    public void PushStartButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+
+}
